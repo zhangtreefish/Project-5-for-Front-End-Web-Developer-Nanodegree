3b06f03910a673724591aa3cd717fd9912367695
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-08-03 22:21:22 -0500

    refactor(index.html&app.js)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var viewModel = function() {
     function initialize() {
+        var myLatLng = new google.maps.LatLng(29.8, 121.5);
+
         var mapOptions = {
-            center: new google.maps.LatLng(29.8, 121.5),
+            center: myLatLng,
             zoom: 8,
             scaleControl: true,
             panControl:true
@@ -9,6 +11,12 @@ var viewModel = function() {
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
         map.style.width='500px';//?
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
     }
     google.maps.event.addDomListener(window, 'load', initialize);
-};
+}();
